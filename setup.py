62e2582389509033932f87e9a1cbb18de4650af5
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2022-10-10 12:10:02 +0200

    Replacing mysqlclient for Mysql-python

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='BarbaraApplication',
       author='Rajagopal M',
       author_email='rajagopal.a.dinesh.28@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
-      install_requires=['gevent', 'Flask>=0.7.2', 'MarkupSafe', 'Flask-SQLAlchemy', 'Flask-Migrate', 'MySQL-Python',
+      install_requires=['gevent', 'Flask', 'MarkupSafe', 'Flask-SQLAlchemy', 'Flask-Migrate', 'mysqlclient',
                         'werkzeug'],
       py_modules=[]
       )
