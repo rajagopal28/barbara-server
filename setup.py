3e6b02c955400e101bf108bc296b68a75a64f4af
Rajagopal M <rajagopal.a.dinesh.28@gmail.com>
2022-10-10 12:06:41 +0200

    Trying to fix build issue due to multiple top level libs

--- setup.py
+++ setup.py
@@ -7,5 +7,6 @@ setup(name='BarbaraApplication',
       author_email='rajagopal.a.dinesh.28@gmail.com',
       url='http://www.python.org/sigs/distutils-sig/',
       install_requires=['gevent', 'Flask>=0.7.2', 'MarkupSafe', 'Flask-SQLAlchemy', 'Flask-Migrate', 'MySQL-Python',
-                        'werkzeug']
+                        'werkzeug'],
+      py_modules=[]
       )
